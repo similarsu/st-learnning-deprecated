818f349b2e0a4bf3aef4c5969e344d4714f3252e
sutong <821192673@qq.com>
2017-06-15 17:34:24 +0800

    add method of print internal state

--- st-learnning-logback/src/main/java/cn/st/learning/logback/HelloWorld.java
+++ st-learnning-logback/src/main/java/cn/st/learning/logback/HelloWorld.java
@@ -1,5 +1,8 @@
 package cn.st.learning.logback;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.status.StatusManager;
+import ch.qos.logback.core.util.StatusPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,5 +13,11 @@ public class HelloWorld {
     public static void main(String []args){
         Logger logger= LoggerFactory.getLogger(HelloWorld.class);
         logger.debug("hello world");
+        printInternalState();
+    }
+
+    private static void printInternalState(){
+        LoggerContext lc= (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(lc);
     }
 }
